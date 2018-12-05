e73a375d358d61d7ba0dcb0e7b296df4018b56fa
yujinshui <602570224@qq.com>
2018-12-05 15:58:38 +0800

    删除属性的静态化，同时新增配置类

---
+++ tencent-recognition/src/main/java/org/face/identify/tencent/config/TencentAccountConfig.java
@@ -0,0 +1,12 @@
+package org.face.identify.tencent.config;
+
+/**
+ * @author code
+ * @Title: TencentAccountConfig
+ * @Copyright: Copyright (c) 2017
+ * @Description: <br>
+ * @Company: www.qdingnet.com
+ * @Created on 2018/12/5上午10:33
+ */
+public interface TencentAccountConfig {
+}
---
+++ tencent-recognition/tencent.md
@@ -0,0 +1,2 @@
+
+腾讯云人脸识别文档地址：[https://cloud.tencent.com/document/product/868/17574](https://cloud.tencent.com/document/product/868/17574)
